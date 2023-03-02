4dc73f38f9f8a87ff0f2d3279a12add52ad6e074
Geeeeorge <Geeeeorge@ishiiryoukanoMacBook-Air.local>
2023-03-02 22:36:49 +0900

    docker整備完了

---
+++ infra/database/mysql.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var mysqlDB *gorm.DB = nil
+
+func NewPostgresDB(dsn string) (*gorm.DB, error) {
+	if mysqlDB != nil {
+		return mysqlDB, nil
+	}
+
+	mysqlDB, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		return nil, err
+	}
+	return mysqlDB, nil
+}

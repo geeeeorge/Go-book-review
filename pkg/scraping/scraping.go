423b184d489e81a13f03e054c8a926472f23f969
Geeeeorge <Geeeeorge@ishiiryoukanoMacBook-Air.local>
2023-03-03 19:00:11 +0900

    impl pkg scraping

---
+++ pkg/scraping/scraping.go
@@ -0,0 +1,64 @@
+package scraping
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type ExtractedData struct {
+	Title string
+	Image []byte
+}
+
+// ScrapingAmazonURL AmazonのURLからExtractedDataをスクレイピング
+func ScrapingAmazonURL(url string) (*ExtractedData, error) {
+	// Request the HTML page.
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Find the data
+	// get Title
+	var title string
+	doc.Find(".a-size-extra-large").Each(func(_ int, s *goquery.Selection) {
+		title = s.Text()
+	})
+
+	// get img
+	var imgUrl string
+	doc.Find(".a-dynamic-image.image-stretch-horizontal.frontImage").Each(func(_ int, s *goquery.Selection) {
+		imgUrl, _ = s.Attr("src")
+	})
+	res, err = http.Get(imgUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("img url status code error: %d %s", res.StatusCode, res.Status)
+	}
+	img, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	data := ExtractedData{
+		Title: strings.TrimSpace(title),
+		Image: img,
+	}
+	return &data, nil
+}
---
+++ pkg/scraping/scraping_test.go
@@ -0,0 +1,50 @@
+package scraping
+
+import (
+	"fmt"
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+func TestScrapingAmazonURL(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		amazonUrl string
+		imgUrl    string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    ExtractedData
+		wantErr bool
+	}{
+		{
+			name: "successful case",
+			args: args{
+				amazonUrl: "https://onl.bz/Xr9qS57",
+			},
+			want: ExtractedData{
+				Title: "カオスエンジニアリング ―回復力のあるシステムの実践",
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			res, err := ScrapingAmazonURL(tc.args.amazonUrl)
+			fmt.Println(res)
+
+			if diff := cmp.Diff(tc.wantErr, err != nil); diff != "" {
+				t.Error(err)
+			}
+			if diff := cmp.Diff(tc.want.Title, res.Title); diff != "" {
+				t.Error(err)
+			}
+		})
+	}
+}
